41ef8964a652a9eb92701991d5a46c7fd7e712bb
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2026-01-03 10:42:41 -0800

    goget: add support for yaml unmarshaling (#19)

--- goget/goget_test.go
+++ goget/goget_test.go
@@ -10,21 +10,26 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert/yaml"
 	"github.com/stretchr/testify/require"
 )
 
 func registerHandlers(mux *http.ServeMux, next http.Handler, specs []Spec) error {
 	for _, spec := range specs {
-		handler, path, err := spec.NewHandler(next)
+		// Not parsed from yaml.
+		if err := spec.SplitHostnamePath(); err != nil {
+			return err
+		}
+		handler, err := spec.NewHandler(next)
 		if err != nil {
 			return err
 		}
-		mux.Handle(path+"/", handler)
-		if len(path) == 0 {
+		mux.Handle(spec.Path()+"/", handler)
+		if spec.Path() == "" {
 			// An empty path will be redirected to /
 			continue
 		}
-		mux.Handle(path, handler)
+		mux.Handle(spec.Path(), handler)
 	}
 	return nil
 }
@@ -293,7 +298,30 @@ func TestGoGetHandlerEdgeCases(t *testing.T) {
 		req := httptest.NewRequest("GET", "http://example.com/pkg?go-get=0", nil)
 		w := httptest.NewRecorder()
 		mux.ServeHTTP(w, req)
-
-		assert.True(t, nextCalled)
+		assert.True(t, nextCalled, "fallback handler should have been called")
 	})
+}
+
+func TestYAMLUnmarshal(t *testing.T) {
+	yamlSpec := `
+- import: example.com/pkg
+  content: example.com/pkg git https://github.com/user/pkg
+- import: example.com/other
+  content: example.com/other git https://github.com/user/other
+`
+	var specs []Spec
+	err := yaml.Unmarshal([]byte(yamlSpec), &specs)
+	require.NoError(t, err)
+
+	require.Len(t, specs, 2)
+
+	assert.Equal(t, "example.com/pkg", specs[0].ImportPath)
+	assert.Equal(t, "example.com/pkg git https://github.com/user/pkg", specs[0].Content)
+	assert.Equal(t, "example.com", specs[0].Hostname())
+	assert.Equal(t, "/pkg", specs[0].Path())
+
+	assert.Equal(t, "example.com/other", specs[1].ImportPath)
+	assert.Equal(t, "example.com/other git https://github.com/user/other", specs[1].Content)
+	assert.Equal(t, "example.com", specs[1].Hostname())
+	assert.Equal(t, "/other", specs[1].Path())
 }
