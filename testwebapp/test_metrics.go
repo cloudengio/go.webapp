fa5627a788468fe6b001895306438c815880bbae
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2026-05-27 16:58:57 -0700

    testwebapp: add support for testing metrics.

---
+++ testwebapp/test_metrics.go
@@ -0,0 +1,56 @@
+// Copyright 2026 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package testwebapp
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"cloudeng.io/logging/ctxlog"
+	"cloudeng.io/sync/errgroup"
+)
+
+type MetricsTest struct {
+	client   *http.Client
+	reporter MetricsReporter
+	specs    []MetricsSpec
+}
+
+type MetricsSpec struct {
+	URL         string   `yaml:"url,omitempty"`
+	MetricNames []string `yaml:"names,omitempty"`
+}
+
+type MetricsReporter func(ctx context.Context, client *http.Client, url string, expectedMetrics []string) (found, missing []string, err error)
+
+func NewMetricsTest(client *http.Client, reporter MetricsReporter, specs ...MetricsSpec) *MetricsTest {
+	return &MetricsTest{
+		client:   client,
+		reporter: reporter,
+		specs:    specs,
+	}
+}
+
+func (m MetricsTest) Run(ctx context.Context) error {
+	if len(m.specs) == 0 {
+		return nil
+	}
+	var g errgroup.T
+	for _, metric := range m.specs {
+		g.Go(func() error {
+			found, missing, err := m.reporter(ctx, m.client, metric.URL, metric.MetricNames)
+			if err != nil {
+				return fmt.Errorf("error checking metrics existence at %v: %v", metric.URL, err)
+			}
+			ctxlog.Info(ctx, "found expected metrics", "url", metric.URL, "metrics", found)
+			if len(missing) > 0 {
+				return fmt.Errorf("some expected metrics were missing: %v", missing)
+			}
+			return nil
+		})
+	}
+	return g.Wait()
+}
